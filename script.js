39a6bce368e869a2e08b617e74dbe017e480d020
Reena Thomas <rtthomas@ucsc.edu>
2024-09-27 12:21:05 -0700

    btn

--- script.js
+++ script.js
@@ -1,75 +1,53 @@
 const response = {
-    stepCount: 4,
-    stepInfo: [
-      {
-        stepNumber: 1,
-        questions: ['step 1 - question 1', 'step 1 - question 2']
-      },
-      {
-        stepNumber: 2,
-        questions: ['step 2 - question 1', 'step 2 - question 2']
-      },
-      {
-        stepNumber: 3,
-        questions: ['step 3 - question 1', 'step 3 - question 2']
-      },
-      {
-        stepNumber: 4,
-        questions: ['step 4 - question 1', 'step 4 - question 2']
-      },
-    ]
-  };
+  stepCount: 4,
+  stepInfo: [
+    {
+      stepNumber: 1,
+      questions: ["step 1 - question 1", "step 1 - question 2"],
+    },
+    {
+      stepNumber: 2,
+      questions: ["step 2 - question 1", "step 2 - question 2"],
+    },
+    {
+      stepNumber: 3,
+      questions: ["step 3 - question 1", "step 3 - question 2"],
+    },
+    {
+      stepNumber: 4,
+      questions: ["step 4 - question 1", "step 4 - question 2"],
+    },
+  ],
+};
 
-
-  let stepNumber=1
-  if (stepNumber === 1) {
-    document.getElementById("previous").disabled=true
-  }
-  else if (stepNumber === response.stepCount) {
-    document.getElementById("next").disabled=true
-  }else{
-    document.getElementById("next").disabled=false
-    document.getElementById("previous").disabled=false
+function updateNavigationButtons() {
+  document.getElementById("previous").disabled = stepNumber === 1;
+  document.getElementById("next").disabled = stepNumber === response.stepCount;
   }
 
-  document.getElementById("question1").innerText = `${response.stepInfo[stepNumber-1].questions[0]}`
-  document.getElementById("question2").innerText = `${response.stepInfo[stepNumber-1].questions[1]}`
+  
+function step() {
+  document.getElementById("question1").innerText = `${
+    response.stepInfo[stepNumber - 1].questions[0]
+  }`;
+  document.getElementById("question2").innerText = `${
+    response.stepInfo[stepNumber - 1].questions[1]
+  }`;
+}
 
+let stepNumber = 1;
+step();
+updateNavigationButtons()
 
+document.getElementById("previous").addEventListener("click", () => {
+  stepNumber--;
+  step();
+  updateNavigationButtons()
+});
 
-  document.getElementById("previous").addEventListener("click", ()=>{
-stepNumber--
-document.getElementById("question1").innerText = `${response.stepInfo[stepNumber-1].questions[0]}`
-  document.getElementById("question2").innerText = `${response.stepInfo[stepNumber-1].questions[1]}`
+document.getElementById("next").addEventListener("click", () => {
+  stepNumber++;
+  step();
+  updateNavigationButtons()
+});
 
-  if (stepNumber === 1) {
-    document.getElementById("previous").disabled=true
-  }
-
-  else if (stepNumber === response.stepCount) {
-    document.getElementById("next").disabled=true
-  }else{
-    document.getElementById("next").disabled=false
-    document.getElementById("previous").disabled=false
-  }
-  })
-
-
-
-  document.getElementById("next").addEventListener("click", ()=>{
-    document.getElementById("previous").disabled=false;
-        stepNumber++
-          document.getElementById("question1").innerText = `${response.stepInfo[stepNumber-1].questions[0]}`
-  document.getElementById("question2").innerText = `${response.stepInfo[stepNumber-1].questions[1]}`
-
-  if (stepNumber === 1) {
-    document.getElementById("previous").disabled=true
-  }
-
-  else if (stepNumber === response.stepCount) {
-    document.getElementById("next").disabled=true
-  }else{
-    document.getElementById("next").disabled=false
-    document.getElementById("previous").disabled=false
-  }
-  })
